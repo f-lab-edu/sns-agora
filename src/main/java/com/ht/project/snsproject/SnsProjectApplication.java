e92394c4b2e019229cf0193e967da70772705761
Hyeontaek Oh <tax941116@gmail.com>
2019-12-31 18:25:45 +0900

    sns 프로젝트 생성

---
+++ src/main/java/com/ht/project/snsproject/SnsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ht.project.snsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsProjectApplication.class, args);
+	}
+
+}
